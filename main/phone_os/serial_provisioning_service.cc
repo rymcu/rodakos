0a720da082fda7ac2042045ed3b5747f9b906da7
ronger <ronger-x@outlook.com>
2026-09-16 09:52:03 +0800

    Fit provisioning task within low internal SRAM

--- main/phone_os/serial_provisioning_service.cc
+++ main/phone_os/serial_provisioning_service.cc
@@ -154,7 +154,7 @@ bool SerialProvisioningService::Start() {
     running_.store(true);
     // Voice wake/AFE reserves most internal SRAM; provisioning only parses a
     // bounded 2 KiB frame and can run with a smaller stack.
-    if (xTaskCreatePinnedToCore(TaskEntry, "serial_prov", 3072, this, 2, &task_,
+    if (xTaskCreatePinnedToCore(TaskEntry, "serial_prov", 2048, this, 2, &task_,
                                 usb_serial_driver_core_) != pdPASS) {
         running_.store(false);
         cloud_refresh_pending_.store(false);
